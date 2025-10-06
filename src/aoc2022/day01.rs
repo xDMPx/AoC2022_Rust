0ac541f4776932dc2b55798a32ba35f9f7eb5bae
xDMPx <57721731+xDMPx@users.noreply.github.com>
2025-10-06 23:10:12 +0200

    Add Day01 part2

--- src/aoc2022/day01.rs
+++ src/aoc2022/day01.rs
@@ -18,3 +18,26 @@ pub fn part01(file_path: &str) -> usize {
     };
     max_calories.try_into().unwrap()
 }
+
+pub fn part02(file_path: &str) -> usize {
+    let puzzle_input: String = std::fs::read_to_string(file_path).unwrap();
+    let calories_list = puzzle_input.lines().map(|l| l.parse::<u64>());
+    let mut elf_calories = vec![];
+    let mut calories_sum = 0;
+    for calories in calories_list {
+        if let Ok(calories) = calories {
+            calories_sum += calories;
+        } else {
+            elf_calories.push(calories_sum);
+            calories_sum = 0;
+        }
+    }
+    elf_calories.push(calories_sum);
+    elf_calories.sort_unstable();
+
+    (elf_calories[elf_calories.len() - 1]
+        + elf_calories[elf_calories.len() - 2]
+        + elf_calories[elf_calories.len() - 3])
+        .try_into()
+        .unwrap()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -8,5 +8,7 @@ mod tests {
     fn test_example_input() {
         let day01_part1 = aoc2022::day01::part01("./test_puzzle_input/day01_test.txt");
         assert_eq!(day01_part1, 24000);
+        let day01_part2 = aoc2022::day01::part02("./test_puzzle_input/day01_test.txt");
+        assert_eq!(day01_part2, 45000);
     }
 }
